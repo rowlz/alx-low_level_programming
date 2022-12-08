4067d58faf79ce4d87b9acd9f6fed0f7e30fd574
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-08 07:34:26 -0800

    Dumping

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry
+ * Return: return 0
+*/
+
+int main(void)
+{
+	char str1[55];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	puts(str1);
+
+	return (0);
+}
