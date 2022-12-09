71354a5755246e00d1b6bf0df1ef610ff2a8b363
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-09 09:05:46 -0800

    Added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ *main - program starts
+ *Description: prints the number
+ *Return: 0 all the time
+*/
+
+int main(void)
+{
+	char x = '0';
+
+	for (; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
