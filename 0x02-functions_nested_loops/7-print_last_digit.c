c7a8fb6d9aef9cdb7042b6edef46f89652c263db
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-13 09:15:04 -0800

    Updated 7-print_last_digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,20 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
-	_putchar(last_digit);
+	int last_digit;
 
+	if( n < 0)
+	{
+		last_digit = (n % 10) * -1;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
 	return (last_digit);
 
 }
