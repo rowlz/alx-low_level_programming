33f121a01291610c2a2cb5b34b115dd0785655f9
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-13 07:58:13 -0800

    Added 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,12 @@
  * _islower - The start of program
  * @c: input
  * Description : prints out the alphabet in lower case
- * Return: 0 if lower case, 0 is upper
+ * Return: 1 if lower case, 0 is upper
 */
 
 int _islower(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c > 96 && c < 123)
 	{
 		return (0);
 	}
