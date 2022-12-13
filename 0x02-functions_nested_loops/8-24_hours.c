228fde96f8b178338840a0c493438ad12a61bd55
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-13 09:41:37 -0800

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * jack_bauer - The start of program
+ * Description : prints out the alphabet in lower case
+ * Return: 1 and print '+'if n greater than zero
+ * 0 and print '0' if zero
+ * -1 and prints '-' if less than zero
+*/
+
+void jack_bauer(void)
+{
+	int h_1 = 0;
+	int h_2 = 0;
+	int m_1 = 0;
+	int m_2 = 0;
+	int minute = 0;
+
+	while (minute < 1440)
+	{
+		_putchar(h_1 + '0');
+		_putchar(h_2 + '0');
+		_putchar(':');
+		_putchar(m_1 + '0');
+		_putchar(m_2 + '0');
+		_putchar('\n');
+
+		m_2++;
+		if (m_2 > 9)
+		{
+			m_2 = 0;
+			m_1++;
+		}
+		if (m_2 > 5)
+		{
+			m_2 = 0;
+			h_2++;
+		}
+		if (h_2 > 9)
+		{
+			h_2 = 0;
+			h_1++;
+		}
+		minute++;
+
+	}
+
+}
