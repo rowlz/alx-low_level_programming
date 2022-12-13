3b242dc9db8dbadbbee1e54effaefce87ff078c1
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-13 10:50:39 -0800

    Addde 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - The start of program
+ * @x: input
+ * @y: input
+ * Description : prints out the alphabet in lower case
+ * Return: 1 and print '+'if n greater than zero
+ * 0 and print '0' if zero
+ * -1 and prints '-' if less than zero
+*/
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+
+}
