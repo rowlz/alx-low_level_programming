892a79ed809e6af225399468c81c746efa1a928e
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-13 09:02:24 -0800

    Aded 7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_last_digit - The start of program
+ * @n: input
+ * Description : prints out the alphabet in lower case
+ * Return: 1 and print '+'if n greater than zero
+ * 0 and print '0' if zero
+ * -1 and prints '-' if less than zero
+*/
+
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	return (last_digit);
+
+}
