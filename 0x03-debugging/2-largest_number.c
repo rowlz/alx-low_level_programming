6ed0dde3f5da7021b596fa1318c162f10108a49b
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-14 04:12:05 -0800

    Added

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * largest_number - tests function that prints
+ * if integer is positive or negative
+ * @a: first integer
+ * @b: Second integer
+ * @c: Third integer
+ * Return: 0
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else if (c > b && c > a)
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints largrsst of 3 numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
