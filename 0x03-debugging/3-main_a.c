1f449aefbca14da10ca3c3188c95f9efa6438cc4
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-14 07:29:19 -0800

    added

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints largrsst of 3 numbers
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_remaining_days - tests function that prints
