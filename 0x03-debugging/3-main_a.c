cabf0a4a0080981e1f8f05fd1acf83e2b2e13a57
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-14 07:13:10 -0800

    Added

---
+++ 0x03-debugging/3-main_a.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - prints largrsst of 3 numbers
+ * leap years into account
+ * Return: 0
+ */
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 4;
+	day = 01;
+	year = 1997;
+
+	printf("Date: %20d/%20d/%40d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
