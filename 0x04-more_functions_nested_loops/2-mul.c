5991a2897594eede1253ce84e56d76f3f6ac77fd
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-15 07:36:09 -0800

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - returms multiplaction
+ * @a: input
+ * @b: input
+ * Return: 0 or 1
+*/
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
