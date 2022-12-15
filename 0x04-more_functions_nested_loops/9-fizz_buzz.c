b1518808aacf4729f5269eed004fb906af2ff34e
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-15 09:57:48 -0800

    Added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 101; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
