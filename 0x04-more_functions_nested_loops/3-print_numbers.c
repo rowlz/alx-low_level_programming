07c46fdd46f489413e892f765a17234c65972166
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-15 07:46:23 -0800

    Added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - returms multiplaction
+ * Return: 0 or 1
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
