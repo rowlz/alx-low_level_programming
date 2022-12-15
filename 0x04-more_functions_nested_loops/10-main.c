286168fefede5759da3789c662d23f2f9c79bec8
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-15 10:17:09 -0800

    added

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - runs the program
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - returns a line
+ * @size: number on lines
+ * Return: 0 or 1
+*/
+
+void print_square(int size)
+{
+	int a;
+	int b;
+	int i;
+	int d;
+	int j;
+
+	a = 0;
+
+	b = size;
+
+	for (i = 0; i < size; i++)
+	{
+		a = a + 1;
+		b = b + 1;
+
+		for (d = b; d > 0; d--)
+		{
+			_putchar(' ');
+		}
+
+		for (j = 0; j < a; j++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
