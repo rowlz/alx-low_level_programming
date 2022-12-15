cca108857c6f9b2f9458bce6fc915778acc847da
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-15 08:46:24 -0800

    Added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - returns a line
+ * @n: number on lines
+ * Return: 0 or 1
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
+	}
+}
