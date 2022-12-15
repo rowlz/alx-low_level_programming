f0b972746ee2890bae0d7099e574e4c95574c4dd
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-15 09:14:38 -0800

    Added 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - returns a line
+ * @n: number on lines
+ * Return: 0 or 1
+*/
+
+void print_line(int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (n > 0)
+	{
+		b = a;
+		while (b > 0)
+		{
+			_putchar(' ');
+			b--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		a++;
+		n--;
+
+	}
+	if (a < 1)
+		_putchar('\n');
+}
