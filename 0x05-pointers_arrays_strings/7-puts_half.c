822f89992bf2071eb7a1a69af29c2fa04ad8755e
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-19 07:09:49 -0800

    add

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - swaps values of two integers
+ * @str: string
+ * Return: Always 0
+*/
+
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+
+	}
+
+	for (i /= 2; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
