970009a11fd30d0f897881d9de99dab7d87bfd4c
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-19 03:59:56 -0800

    added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - swaps values of two integers
+ * @str: string
+ * Return: Always 0
+*/
+
+void _puts(char *str)
+{
+	printf("%s,\n" str);
+}
