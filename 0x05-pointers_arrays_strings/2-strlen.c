8f23fe63e100b15851af056d5e290854febd6e40
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-19 03:45:33 -0800

    Added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - swaps values of two integers
+ * @s: string
+ * Return: Always 0
+*/
+
+int _strlen(char *s)
+{
+	char new_string[] = s;
+	int a;
+
+	for (a = 0; new_string[a] != '\0'; ++a)
+	{
+
+	}
+	return (a);
+}
