927620d8ecf7e5c0776d56ade07de984b6ab28c3
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-19 06:55:48 -0800

    Add

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - prints a string in reverse
+ * @str: string
+ * Return: Always 0
+*/
+
+void puts2(char *str)
+{
+	int a;
+	int b;
+	int length;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+
+	}
+
+	length = a;
+
+	for (b = 0; b <= length; b++)
+	{
+		if (b % 2 == 0)
+		{
+			_putchar(str[b])
+		}
+	}
+
+	_putchar('\n');
+}
