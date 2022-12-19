9d42c88a08e0a95eb72cc1038c041869177da8d9
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-19 07:31:34 -0800

    add

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,14 @@
 void print_array(int *a, int n)
 {
 	int b;
+
 	b = 0;
 
-	for (n--; n >= 0; n--)
+
+	for (n--; n >= 0; n--, b++)
 	{
 		printf("%d", a[b]);
-		if(n > 0)
+		if (n > 0)
 		{
 			printf(", ");
 		}
