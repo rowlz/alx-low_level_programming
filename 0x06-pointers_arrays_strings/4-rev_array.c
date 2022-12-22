259a4979dc3e60b6b2988e2cb1f3acd0d175a09b
Rowland Oladiran <rowlandoladiran3@gmail.com>
2022-12-22 08:22:03 -0800

    add

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * reverse_array - concat two strings
+ * @a: 1
+ * @n: 2
+ * Return: 0
+*/
+
+void reverse_array(int *a, int n)
+{
+	int b;
+	int hold;
+
+	for (b = 0; b < (n / 2); a++)
+	{
+		hold = a[b];
+		a[b] = a[n - b - 1];
+		a[n - b - 1] = hold;
+	}
+
+
+}
