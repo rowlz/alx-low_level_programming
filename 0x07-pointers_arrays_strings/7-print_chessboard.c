a87429050967cdfdef430c5a9aecd62eac7ed499
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-02 06:17:41 -0800

    Added 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_chessboard - print chessbrd
+ * @a: 2d arrays
+ */
+
+void print_chessboard(char (*a)[8]);
+{
+	int r, c;
+
+	r = 0;
+	while (r < 8)
+	{
+		c = 0;
+		while (c < 8)
+		{
+			_putchar(a[r][c]);
+			c++;
+		}
+		_putchar('\n');
+		r++;
+
+	}
+}
