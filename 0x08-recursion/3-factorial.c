cf9ba6a89dcfbb6e3362510b9fcfd1267e5148a5
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-04 07:21:33 -0800

    Added

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - prints out a string with recursion
+ * @n: string
+ * Return: The string value
+ */
+
+int factorial(int n);
+{
+	int fact = 0;
+
+	if (n < 0)
+		return (-1);
+	else if (n < 2)
+		return (n);
+	fact = n * factorial(n -1);
+
+	return (fact);
+}
