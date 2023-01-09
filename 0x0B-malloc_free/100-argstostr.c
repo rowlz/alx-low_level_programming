af7849b677defde2993d8f2b8f310dba521134d8
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-09 12:40:40 -0800

    Addd

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * argstostr - returns con argu
+ * @ac: count
+ * @av: vectors
+ * Return: the value 0
+*/
+
+char *argstostr(int ac, char **av)
+{
+	(void) ac;
+	(void) av;
+}
