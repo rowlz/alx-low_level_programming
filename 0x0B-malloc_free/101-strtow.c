7e2693449a75c0998f0dbc468d4a5ddd440e8611
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-09 12:44:42 -0800

    Add

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * strtow - returns con argu
+ * @str: count
+ * Return: the value 0
+*/
+
+char **strtow(char *str)
+{
+	(void) str;
+
+	return (0);
+}
