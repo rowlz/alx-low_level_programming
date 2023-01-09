ba6427d4995dffe314d1ae5da770c2639f60db8f
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-09 07:57:34 -0800

    Added 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - Returns a atring to a new memory
+ * @str: The size of the array
+ * Return: Null is str = NULL or insufficient memory
+*/
+
+char *_strdup(char *str);
+{
+	int dup_string_len = strlen(str);
+	char *dup_string = (char *)malloc(sizeof(char) * dup_string_len);
+
+	if (str == NULL || dup_string == NULL)
+	{
+		return (NULL);
+	}
+
+	return (dup_string);
+}
