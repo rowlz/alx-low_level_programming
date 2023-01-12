1f4b9010854c1a6c53b754c9623fae5142005c4e
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-12 10:53:51 -0800

    add

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * realloc - reallocating a memory
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: mew size
+ * Return: the new memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	(void) ptr;
+	(void) old_size;
+	(void) new_size;
+
+	return (0);
+}
