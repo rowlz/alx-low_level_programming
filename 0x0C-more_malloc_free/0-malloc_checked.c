e6722ebf33edc886d5fc09301d294ce86f540412
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-12 04:01:59 -0800

    Added 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * malloc_checked - a function that allocates memory
+ * @b: the amount of memory to br created
+ * Return: if faliure oricess termmination of 98
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *new_mem = (int *)malloc(sizeof(int) * b);
+
+	if (new_mem == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (new_mem);
+	}
+}
