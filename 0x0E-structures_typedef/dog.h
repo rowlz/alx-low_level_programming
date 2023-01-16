53cf1354d671ebc226922e9598da51639653e3ac
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-16 07:23:43 -0800

    Added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_HEADER
+#define _DOG_HEADER
+
+
+
+/**
+ * struct dog - creating a new structure of type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
