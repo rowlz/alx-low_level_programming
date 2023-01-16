ba681366ee4ee57576b5d4bcfb3a590696d2205c
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-16 07:30:19 -0800

    aadd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,12 @@
 #ifndef _DOG_HEADER
 #define _DOG_HEADER
 
-
-
 /**
  * struct dog - creating a new structure of type dog
  * @name: name of dog
  * @age: age of dog
  * @owner: name of owner
- */
+*/
 
 typedef struct dog
 {
