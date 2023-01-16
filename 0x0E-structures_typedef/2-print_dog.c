2b2ea56eee72327101b3ef10187e16f26a3fc8f5
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-16 07:54:39 -0800

    added 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - inititalize data of type dog
+ * @d: structure type of dog
+ * Return: 0
+*/
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %d\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
