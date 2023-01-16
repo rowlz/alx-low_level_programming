7af6ca1af648c4f858a5ad8abcd234aa31b0fe34
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-16 05:25:45 -0800

    Added 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - A program that prints the name of a file
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
