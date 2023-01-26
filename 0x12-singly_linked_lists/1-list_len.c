91379c8f807b4a32302e455dfc8e0647afab59d0
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-26 08:19:45 -0800

    Adde

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * list_len - prints the number of elements of a list
+ * @h: a list parameter
+ * Return: the number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
