313ab09b133473f201366bb939dbea0062ea46bc
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-30 08:20:56 -0800

    add

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - print integers in the list
+ * @head: linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
