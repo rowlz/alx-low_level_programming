02be97f307beed947682825df9a9a3fc1acd704f
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-30 08:33:38 -0800

    add new code

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - print integers in the list
+ * @head: linked list
+ * @index: index
+ * Return: a head note
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp = head;
+	unsigned int count = 0;
+
+	while (tmp && count < index)
+	{
+		tmp = tmp->next;
+		count++
+	}
+
+	return (tmp != NULL ? tmp : NULL);
+}
