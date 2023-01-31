9da58838a9d5f40a877c3532afb951e7c8aa89c2
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-31 08:28:28 -0800

    add

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (tmp && count < index)
 	{
 		tmp = tmp->next;
-		count++
+		count++;
 	}
 
 	return (tmp != NULL ? tmp : NULL);
