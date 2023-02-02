007ad8861680e8cb54afb09f883c1ae4f956760f
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-02-02 06:05:00 -0800

    Added task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - print binary of a decimal
+ * @n: number
+ * @index: index
+ * Return: value bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
