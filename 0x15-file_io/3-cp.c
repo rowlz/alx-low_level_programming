227e60fe7033ed924e1378dc3698afb3bcadd609
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-02-06 07:12:55 -0800

    added task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main
+ * @ac: arguement count
+ * @av: arguement vector
+ * Return: 0
+ */
+
+int main(int ac, char *av[])
+{
+	(void)ac;
+	(void)av;
+
+	return (0);
+}
